a450cc1f31fbf0473d011b509dc762bf9f492539
Xo4yxa <serj.kolodin2014@yandex.ru>
2022-05-26 22:24:42 +0300

    ButtonIcons

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { Up } from '../components';
 import { AppContextProvider, IAppContext } from '../context/app.context';
 import { Footer } from './Footer/Footer';
 import { Header } from './Header/Header';
@@ -15,6 +16,7 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
 				{children}
 			</div>
 			<Footer className={styles.footer} />
+			<Up />
 		</div>
 	);
 };
---
+++ components/Up/Up.tsx
@@ -0,0 +1,34 @@
+import styles from './Up.module.css';
+import UpIcon from './up.svg'
+import cn from 'classnames';
+import { useScrollY } from '../../hooks/useScrollY';
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect } from 'react';
+import { ButtonIcon } from '../ButtonIcon/ButtonIcon';
+
+export const Up = (): JSX.Element => {
+	const controls = useAnimation();
+	const y = useScrollY();
+
+	useEffect(() => {
+		controls.start({ opacity: y / document.body.scrollHeight })
+	}, [y, controls])
+
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth'
+		})
+	}
+
+
+	return (
+		<motion.div
+			className={styles.up}
+			animate={controls}
+			initial={{ opacity: 0 }}
+		>
+			<ButtonIcon appearance='primary' icon='up' onClick={scrollToTop} />
+		</motion.div>
+	)
+};
