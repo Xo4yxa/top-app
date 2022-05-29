4889640e4691a5541b0e78482b2a1746213523e4
Xo4yxa <serj.kolodin2014@yandex.ru>
2022-05-29 21:58:21 +0300

    MobileMenu

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,11 +2,15 @@ import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 import ArrowIcon from './arrow.svg'
 import cn from 'classnames';
+import { motion, useMotionValue } from 'framer-motion';
+import { useEffect } from 'react';
 
 
 export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
+
 	return (
-		<button
+		<motion.button
+			whileHover={{ scale: 1.05 }}
 			className={cn(styles.button, className, {
 				[styles.primary]: appearance == 'primary',
 				[styles.ghost]: appearance == 'ghost'
@@ -19,6 +23,6 @@ export const Button = ({ appearance, arrow = 'none', children, className, ...pro
 			})}>
 				<ArrowIcon />
 			</span>}
-		</button >
+		</motion.button >
 	)
 };
